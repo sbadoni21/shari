b126ea9cd188a7e4b570691f2c0944410a2c5410
sbadoni21 <97601162+sbadoni21@users.noreply.github.com>
2024-04-26 13:58:28 +0530

    sdfsgsdf

--- app/layout.js
+++ app/layout.js
@@ -45,9 +45,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <Script id='google-analytics'>
-       {` window.dataLayer = window.dataLayer || []; function gtag()
-        {dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config',
-        'G-P8TRPYMTZR');`}
+       {`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9J6HVY4E09');`}
       </Script>
       <Script async src="https://www.googletagmanager.com/gtag/js?id=G-P8TRPYMTZR"></Script></head>
             
