c21cf1478f7eccda946b55b40a0cd4521c856df4
sbadoni21 <97601162+sbadoni21@users.noreply.github.com>
2024-04-29 10:47:53 +0530

    new

--- components/frontend/Instagram.jsx
+++ components/frontend/Instagram.jsx
@@ -47,7 +47,8 @@ const Instagram = () => {
       </div>
       <Space16 />
       <Space16 />
-      <div className="flex flex-wrap justify-center gap-4 min-h-fit w-full mb-10">
+<div class="elfsight-app-45e4025a-100a-48c0-9bf6-d8f9095feda1" data-elfsight-app-lazy></div>
+      {/* <div className="flex flex-wrap justify-center gap-4 min-h-fit w-full mb-10">
         {showSmallScreen ? (
          <div className=' flex overflow-x-auto '>
             {photos.slice(0, 2).map((photo) => (
@@ -89,7 +90,7 @@ const Instagram = () => {
             </div>
           ))
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
